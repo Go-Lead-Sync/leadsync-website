dc11cbd29ff42c7e8403586b30bddabd58b3a4ca
Kurt <kurt@goleadsyncs.com>
2026-02-03 23:00:15 +0200

    Add GitHub Pages deploy workflow

---
+++ next.config.js
@@ -0,0 +1,10 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
+};
+
+module.exports = nextConfig;
